46e383953e85a2ea98de21ebd649131dcf58fea2
HighSunday <brian17560@gmail.com>
2021-03-24 17:27:55 +0800

    動畫功能

--- main.m
+++ main.m
@@ -1,6 +1,6 @@
 
 %========CFG========
-K=3
+K=2
 iteration=10
 %===================
 
@@ -16,10 +16,18 @@ for i=1:iteration
     %分群
     res=run.Clustering(data);
     run.Plot_2d_df(res);
-
+    
+    last_points=run.points;
+	drawnow				% 立即作圖
+    pause(1)
+    
     % 重新定位群心
     run.recalculate_points(res);
     run.Plot_2d_df(res);
+    
+    if(run.points==last_points)
+        break
+    end
 end
 
 %%% 3d case
