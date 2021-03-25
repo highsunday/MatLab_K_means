e8389f5046e7e42beff49898200eff75a9e273c8
HighSunday <brian17560@gmail.com>
2021-03-25 09:48:24 +0800

    3d 完成

--- main.m
+++ main.m
@@ -1,36 +1,58 @@
 
 %========CFG========
-K=4
+K=3
 iteration=100
-is_2D=true % 3D為false
+is_2D=false % 2D為 true ; 3D為 false
 %SepalLengthCm,SepalWidthCm,PetalLengthCm,PetalWidthCm
-col1='SepalLengthCm'
+col1='PetalLengthCm'
 col2='PetalWidthCm'
+col3='SepalWidthCm'
 %===================
 
-
 % initial
-run=K_means(K,col1,col2);
-data=run.Output_2d_df();
-run.initialPoints(data);
+if is_2D
+    run=K_means(K,col1,col2);
+    data=run.Output_df();
+    run.initialPoints(data);
 
-%分群
-res=run.Clustering(data);
-run.Plot_2d_df(res);
-
-for i=1:iteration
-    last_points=run.points;
-	drawnow				% 立即作圖
-    pause(1)
-    
-    % 重新定位群心
-    run.recalculate_points(res);
-    run.Plot_2d_df(res);
+    %分群
     res=run.Clustering(data);
-    if(run.points==last_points)
-        break
+    run.Plot_df(res);
+
+    for i=1:iteration
+        last_points=run.points;
+        drawnow				% 立即作圖
+        pause(1)
+
+        % 重新定位群心
+        run.recalculate_points(res);
+        run.Plot_df(res);
+        res=run.Clustering(data);
+        if(run.points==last_points)
+            break
+        end
+    end
+else
+    run=K_means(K,col1,col2,col3);
+    data=run.Output_df();
+    run.initialPoints(data);
+    res=run.Clustering(data);
+    run.Plot_df(res);
+    for i=1:iteration
+        last_points=run.points;
+        drawnow				% 立即作圖
+        pause(1)
+
+        % 重新定位群心
+        run.recalculate_points(res);
+        run.Plot_df(res);
+        res=run.Clustering(data);
+        if(run.points==last_points)
+            break
+        end
     end
 end
+
 
 %%% 3d case
 %K_means.Plot_2d_df(data)
